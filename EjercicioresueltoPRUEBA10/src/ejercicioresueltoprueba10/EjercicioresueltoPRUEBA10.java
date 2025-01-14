e56f980be15d4cc7d76e045304fcf89bb67c4890
JuanPa0109 <jusanchezre@unal.edu.co>
2025-01-13 22:52:51 -0500

    Add files via upload

---
+++ EjercicioresueltoPRUEBA10/src/ejercicioresueltoprueba10/EjercicioresueltoPRUEBA10.java
@@ -0,0 +1,14 @@
+
+package ejercicioresueltoprueba10;
+
+
+public class EjercicioresueltoPRUEBA10 {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
---
+++ PoofigurasPRUEBA/src/poofigurasprueba/PoofigurasPRUEBA.java
@@ -0,0 +1,14 @@
+
+package poofigurasprueba;
+
+
+public class PoofigurasPRUEBA {
+
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+    
+}
